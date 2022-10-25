f52e7268aac4000dfaed4d36f1ae7de186d959a0
Matthew Wiggins <mattwiggins5@gmail.com>
2022-10-25 11:38:28 +0100

    project setup up

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+      ],
+    };
+  },
+};
+</script>
